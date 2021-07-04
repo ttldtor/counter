9b97b2fb7afce4866221874af60089782c69ca8d
Anatoly Kalin <ttldtor@gmail.com>
2021-07-04 23:50:29 +0300

    CommandParser

---
+++ src/CommandParser.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <string>
+#include <boost/algorithm/string.hpp>
+
+namespace cnt {
+struct NoneCommand {};
+struct UnknownCommand {};
+struct PrintHelpCommand {};
+struct PrintRecordsCommand {};
+struct QuitCommand {};
+struct AddRecordCommand {
+  std::string recordName{};
+
+  explicit AddRecordCommand(std::string s) : recordName{std::move(s)} {}
+};
+
+struct RemoveRecordCommand {
+  std::string recordName{};
+
+  explicit RemoveRecordCommand(std::string s) : recordName{std::move(s)} {}
+};
+
+struct DumpRecordsCommand {
+  std::string fileName{};
+
+  explicit DumpRecordsCommand(std::string s) : fileName{std::move(s)} {}
+};
+
+struct LoadRecordsCommand {
+  std::string fileName{};
+
+  explicit LoadRecordsCommand(std::string s) : fileName{std::move(s)} {}
+};
+
+using Command = std::variant<NoneCommand, UnknownCommand, PrintHelpCommand, PrintRecordsCommand, QuitCommand,
+                             AddRecordCommand, RemoveRecordCommand, DumpRecordsCommand, LoadRecordsCommand>;
+
+struct CommandParser {
+  Command operator()(const std::string &commandString) const {
+    if (commandString.empty()) return NoneCommand{};
+
+    if (commandString == "?") { return PrintHelpCommand{}; }
+
+    if (commandString == "!") { return PrintRecordsCommand{}; }
+
+    if (commandString == "q") return QuitCommand{};
+
+    if (commandString.size() < 3) { return UnknownCommand{}; }
+
+    if (commandString.starts_with("+ ")) { return AddRecordCommand{boost::trim_copy(commandString.substr(2))}; }
+
+    if (commandString.starts_with("- ")) { return RemoveRecordCommand{boost::trim_copy(commandString.substr(2))}; }
+
+    if (commandString.starts_with("d ")) { return DumpRecordsCommand{boost::trim_copy(commandString.substr(2))}; }
+
+    if (commandString.starts_with("l ")) { return LoadRecordsCommand{boost::trim_copy(commandString.substr(2))}; }
+
+    return UnknownCommand{};
+  }
+};
+}// namespace cnt
--- src/main.cpp
+++ src/main.cpp
@@ -6,231 +6,210 @@
 #include <iostream>
 #include <string_view>
 #include <unordered_map>
+#include <utility>
+#include <variant>
 #include <vector>
 
+#include "CommandParser.hpp"
+
+namespace cnt {
 struct Counter {
-    enum class ProcessingStatus { None,
-                                  Quit,
-                                  Ok,
-                                  Error };
+  enum class ProcessingStatus { None, Quit, Ok, Error };
 
-    struct Record {
-        std::string name{};
-        std::size_t count{};
+  struct Record {
+    std::string name{};
+    std::size_t count{};
 
-        [[nodiscard]] std::string toString() const {
-            return fmt::format("{} - {}", name, count);
-        }
+    [[nodiscard]] std::string toString() const { return fmt::format("{} - {}", name, count); }
 
-        bool fromString(const std::string &str) {
-            std::vector<std::string> split = {};
+    bool fromString(const std::string &str) {
+      std::vector<std::string> split = {};
 
-            boost::algorithm::split_regex(split, str, boost::regex(R"(\s+-\s+)"));
+      boost::algorithm::split_regex(split, str, boost::regex(R"(\s+-\s+)"));
 
-            if (split.size() == 2) {
-                name = split[0];
-                count = static_cast<std::size_t>(std::stoull(split[1]));
+      if (split.size() == 2) {
+        name = split[0];
+        count = static_cast<std::size_t>(std::stoull(split[1]));
 
-                return true;
-            }
+        return true;
+      }
 
-            return false;
-        }
-
-        friend std::ostream &operator<<(std::ostream &stream, const Counter::Record &record) {
-            stream << record.toString();
-
-            return stream;
-        }
-    };
-
-    std::unordered_map<std::string, std::size_t> data{};
-
-    std::string makeDump() {
-        std::vector<Record> records;
-        std::string result;
-
-        for (auto [k, v] : data) {
-            records.emplace_back(Record{k, v});
-        }
-
-        std::sort(records.begin(), records.end(), [](const Record &r1, const Record &r2) {
-            return (r1.count > r2.count) || (r1.count == r2.count && r1.name < r2.name);
-        });
-
-        for (const auto &r : records) {
-            result += fmt::format("{}\n", r.toString());
-        }
-
-        return result;
+      return false;
     }
 
-    static void printHelp() {
-        fmt::print("Commands:\n"
-                   "  ? : Print this help\n"
-                   "  ! : Print the current records dump\n"
-                   "  q : Exit\n"
-                   "  + <record> : Add the record\n"
-                   "  - <record> : Remove the record\n"
-                   "  d - <file name> : Dump all records to the file\n"
-                   "  l - <file name> : Load records from the file\n\n");
+    friend std::ostream &operator<<(std::ostream &stream, const Counter::Record &record) {
+      stream << record.toString();
+
+      return stream;
     }
+  };
 
-    void printDump() {
-        std::cout << makeDump();
-    }
+  std::unordered_map<std::string, std::size_t> data{};
 
-    ProcessingStatus processCommand(const std::string &command) {
-        if (command.empty()) return ProcessingStatus::None;
+  std::string makeDump() {
+    std::vector<Record> records;
+    std::string result;
 
-        if (command == "?") {
-            printHelp();
+    for (auto [k, v] : data) { records.emplace_back(Record{k, v}); }
 
-            return ProcessingStatus::Ok;
-        }
+    std::sort(records.begin(), records.end(), [](const Record &r1, const Record &r2) {
+      return (r1.count > r2.count) || (r1.count == r2.count && r1.name < r2.name);
+    });
 
-        if (command == "!") {
-            printDump();
+    for (const auto &r : records) { result += fmt::format("{}\n", r.toString()); }
 
-            return ProcessingStatus::Ok;
-        }
+    return result;
+  }
 
-        if (command == "q") return ProcessingStatus::Quit;
+  static void printHelp() {
+    fmt::print("Commands:\n"
+               "  ? : Print this help\n"
+               "  ! : Print the current records dump\n"
+               "  q : Exit\n"
+               "  + <record> : Add the record\n"
+               "  - <record> : Remove the record\n"
+               "  d - <file name> : Dump all records to the file\n"
+               "  l - <file name> : Load records from the file\n\n");
+  }
 
-        if (command.size() < 2) {
-            printHelp();
+  void printDump() { std::cout << makeDump(); }
 
-            return ProcessingStatus::Error;
-        }
+  ProcessingStatus processCommand(const std::string &command) {
+    return std::visit(
+      [this](auto &&arg) {
+        using T = std::decay_t<decltype(arg)>;
 
-        if (command.starts_with("+ ")) {
-            add(command.substr(2));
-            printDump();
+        if constexpr (std::is_same_v<T, NoneCommand>) {
+          return ProcessingStatus::None;
+        } else if constexpr (std::is_same_v<T, PrintHelpCommand>) {
+          printHelp();
 
-            return ProcessingStatus::Ok;
-        }
+          return ProcessingStatus::Ok;
+        } else if constexpr (std::is_same_v<T, PrintRecordsCommand>) {
+          printDump();
 
-        if (command.starts_with("- ")) {
-            remove(command.substr(2));
-            printDump();
+          return ProcessingStatus::Ok;
+        } else if constexpr (std::is_same_v<T, QuitCommand>) {
+          return ProcessingStatus::Quit;
+        } else if constexpr (std::is_same_v<T, AddRecordCommand>) {
+          add(arg.recordName);
+          printDump();
 
-            return ProcessingStatus::Ok;
-        }
+          return ProcessingStatus::Ok;
+        } else if constexpr (std::is_same_v<T, RemoveRecordCommand>) {
+          remove(arg.recordName);
+          printDump();
 
-        if (command.starts_with("d ")) {
-            dump(command.substr(2));
+          return ProcessingStatus::Ok;
+        } else if constexpr (std::is_same_v<T, DumpRecordsCommand>) {
+          dump(arg.fileName);
 
-            printDump();
+          printDump();
 
-            return ProcessingStatus::Ok;
-        }
+          return ProcessingStatus::Ok;
+        } else if constexpr (std::is_same_v<T, LoadRecordsCommand>) {
+          auto result = load(arg.fileName);
 
-        if (command.starts_with("l ")) {
-            if (load(command.substr(2)) == ProcessingStatus::Ok) {
-                printDump();
+          printDump();
 
-                return ProcessingStatus::Ok;
-            }
-
-            printDump();
-
-            return ProcessingStatus::Error;
+          return result;
         }
 
         printHelp();
 
-        return ProcessingStatus::Ok;
+        return ProcessingStatus::Error;
+      },
+      CommandParser{}(command));
+  }
+
+  void add(const std::string &name) {
+    auto it = data.find(name);
+
+    if (it == data.end()) {
+      data[name] = 1;
+
+      fmt::print("Added the '{}' record\n", name);
+    } else {
+      it->second++;
+
+      fmt::print("Incremented the '{}' record\n", name);
+    }
+  }
+
+  void remove(const std::string &name) {
+    auto it = data.find(name);
+
+    if (it != data.end()) {
+      it->second--;
+
+      if (it->second == 0) {
+        data.erase(it);
+
+        fmt::print("Removed the '{}' record\n", name);
+      } else {
+        fmt::print("Decremented the '{}' record\n", name);
+      }
+    }
+  }
+
+  void dump(const std::string &fileName) {
+    std::ofstream os{fileName};
+
+    os << makeDump();
+
+    fmt::print("Dumped: {}\n", data.size());
+  }
+
+  ProcessingStatus load(const std::string &fileName) {
+    std::ifstream is(fileName);
+
+    if (!is.is_open()) {
+      fmt::print("Can't open file: {}\n", fileName);
+
+      return ProcessingStatus::Error;
     }
 
-    void add(const std::string &name) {
-        auto it = data.find(name);
+    std::unordered_map<std::string, std::size_t> tempData{};
 
-        if (it == data.end()) {
-            data[name] = 1;
+    for (std::string record{}; std::getline(is, record);) {
+      Record r;
+      auto recordString = boost::trim_copy(record);
 
-            fmt::print("Added the '{}' record\n", name);
-        } else {
-            it->second++;
+      if (recordString.empty()) { continue; }
 
-            fmt::print("Incremented the '{}' record\n", name);
-        }
-    }
-
-    void remove(const std::string &name) {
-        auto it = data.find(name);
-
-        if (it != data.end()) {
-            it->second--;
-
-            if (it->second == 0) {
-                data.erase(it);
-
-                fmt::print("Removed the '{}' record\n", name);
-            } else {
-                fmt::print("Decremented the '{}' record\n", name);
-            }
-        }
-    }
-
-    void dump(const std::string &fileName) {
-        std::ofstream os{fileName};
-
-        os << makeDump();
-
-        fmt::print("Dumped: {}\n", data.size());
-    }
-
-    ProcessingStatus load(const std::string &fileName) {
-        std::ifstream is(fileName);
-
-        if (!is.is_open()) {
-            fmt::print("Can't open file: {}\n", fileName);
-
-            return ProcessingStatus::Error;
-        }
-
-        std::unordered_map<std::string, std::size_t> tempData{};
-
-        for (std::string record{}; std::getline(is, record);) {
-            Record r;
-            auto recordString = boost::trim_copy(record);
-
-            if (recordString.empty()) {
-                continue;
-            }
-
-            if (r.fromString(recordString)) {
-                tempData[r.name] = r.count;
-            } else {
-                is.close();
-                fmt::print("Error while reading the file: {}, error record: '{}'\n", fileName, recordString);
-
-                return ProcessingStatus::Error;
-            }
-        }
-
+      if (r.fromString(recordString)) {
+        tempData[r.name] = r.count;
+      } else {
         is.close();
+        fmt::print("Error while reading the file: {}, error record: '{}'\n", fileName, recordString);
 
-        data = tempData;
-        fmt::print("The dump has been loaded.\n");
-
-        return ProcessingStatus::Ok;
+        return ProcessingStatus::Error;
+      }
     }
+
+    is.close();
+
+    data = tempData;
+    fmt::print("The dump has been loaded.\n");
+
+    return ProcessingStatus::Ok;
+  }
 };
+}
 
 int main() {
-    Counter counter{};
+  cnt::Counter counter{};
 
-    Counter::printHelp();
+  cnt::Counter::printHelp();
 
-    for (;;) {
-        std::cout << "Counter> " << std::flush;
+  for (;;) {
+    std::cout << "Counter> " << std::flush;
 
-        std::string command{};
-        std::getline(std::cin, command);
+    std::string command{};
+    std::getline(std::cin, command);
 
-        if (counter.processCommand(boost::trim_copy(command)) == Counter::ProcessingStatus::Quit) break;
-    }
+    if (counter.processCommand(boost::trim_copy(command)) == cnt::Counter::ProcessingStatus::Quit) break;
+  }
 
-    return 0;
+  return 0;
 }
